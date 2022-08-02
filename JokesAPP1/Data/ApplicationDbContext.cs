b67c480d3623e82c59a4f45c482198abf9f2107c
Praneeth-kallam <ydantano@asu.edu>
2022-08-02 11:56:53 -0700

    first

---
+++ JokesAPP1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesAPP1.Models;
+
+namespace JokesAPP1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesAPP1.Models.Joke>? Joke { get; set; }
+    }
+}
